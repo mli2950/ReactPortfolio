d6b60ad6d0ec117daec82c6ff7949164dbe63c55
Michael Li <Mli2950@gmail.com>
2021-03-04 22:09:03 -0700

    Fixed footer spacing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
 import About from "./components/about";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Navbar from "./components/reusable/navbar";
+import Footer from "./components/reusable/footer";
+import Projects from "./components/projects";
 import "./App.css";
 
 function App() {
-  return <About />;
+  return (
+    <div className="App">
+      <HashRouter basename="/">
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          {/* <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} /> */}
+        </Switch>
+      </HashRouter>
+    </div>
+  );
 }
 
 export default App;
